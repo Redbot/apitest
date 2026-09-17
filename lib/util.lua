4fc8ddea9eab54cb5afdbe72fc01d97ab7adf51d
Redbot <ok@trevormail.com>
2026-09-17 01:04:14 +0000

    Friendlier greeting

--- lib/util.lua
+++ lib/util.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 function M.greet(name)
-    return string.format("\\ay[apitest]\\ax hello, %s", name)
+    return string.format("\\ay[apitest]\\ax hello there, %s", name)
 end
 
 return M
